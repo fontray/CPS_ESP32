fc0540e0033545da19b5f18380ac9a17bf4cc1c7
fontray <100141401+fontray@users.noreply.github.com>
2024-03-13 17:45:03 +0800

    Update and rename ESP32_2.1.ino to ESP32_2_1.ino

--- LAB2/DHT11/ESP32_2_1.ino
+++ LAB2/DHT11/ESP32_2_1.ino
@@ -7,14 +7,12 @@
 DHT dht(DHT_PIN, DHT_TYPE); //宣告建立DHT類別物件
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   dht.begin(); //dht初始化
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   vTaskDelay(2000 / portTICK_PERIOD_MS); //暫停2秒
   float h = dht.readHumidity(); // Read humidity as Celsius (the default)
   float t = dht.readTemperature(); // Read temperature as Celsius (the default)
